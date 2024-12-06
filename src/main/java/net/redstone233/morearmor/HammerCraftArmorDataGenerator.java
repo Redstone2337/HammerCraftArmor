b41d552f8faec86dd02600b769eaeeddedf338a1
Redstone2337 <3356168312@qq.com>
2024-12-06 10:32:02 +0800

    Add files via upload

--- src/main/java/net/redstone233/morearmor/HammerCraftArmorDataGenerator.java
+++ src/main/java/net/redstone233/morearmor/HammerCraftArmorDataGenerator.java
@@ -4,6 +4,7 @@ import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
 import net.redstone233.morearmor.data.ModEnglishLangProvider;
 import net.redstone233.morearmor.data.ModModelsProvider;
+import net.redstone233.morearmor.data.ModRecipesProvider;
 
 public class HammerCraftArmorDataGenerator implements DataGeneratorEntrypoint {
 	@Override
@@ -12,6 +13,7 @@ public class HammerCraftArmorDataGenerator implements DataGeneratorEntrypoint {
 
 		pack.addProvider(ModModelsProvider::new);
 		pack.addProvider(ModEnglishLangProvider::new);
+		pack.addProvider(ModRecipesProvider::new);
 		//pack.addProvider(ModENUSlangProvider::new);
 	}
 }
